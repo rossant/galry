0de4af4f5fdd40b42f74f8b65babb45effa0c617
Cyrille Rossant <->
2012-10-31 16:27:01 +0000

    fixed benchmark01_points.py

--- benchmarks/benchmark01_points.py
+++ benchmarks/benchmark01_points.py
@@ -6,19 +6,14 @@ from galry import *
 import numpy.random as rdn
 
 # Number of points.
-N = 100000000
+N = 1000000
 
 class MyPaintManager(PaintManager):
     def initialize(self):
         self.parent.display_fps = True
         self.interaction_manager.constrain_navigation = False
-        
         data = 0.2 * rdn.randn(N, 2)
-        
-        color = rdn.rand(N, 4)
-        
-        self.create_dataset(PlotTemplate, size=len(data), single_color=False)
-        self.set_data(position=data, color=color)
-        
+        self.create_dataset(PlotTemplate, size=len(data))
+        self.set_data(position=data)
         
 show_basic_window(paint_manager=MyPaintManager)
