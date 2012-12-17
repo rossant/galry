c8a408c174d0d8050024dd7f164085b05c9b07c4
Cyrille Rossant <cyrille.rossant@github.com>
2012-12-17 17:06:05 +0000

    updated __init__.py

--- galry/__init__.py
+++ galry/__init__.py
@@ -3,7 +3,7 @@
 from debugtools import *
 
 # import PyQT or Pyside
-from python_qt_binding import QtCore, QtDeclarative, QtGui
+from python_qt_binding import QtCore, QtGui
     
 from colors import *
 from manager import *
