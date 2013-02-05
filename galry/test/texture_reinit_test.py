5e21d095b1463583091f0e37a2e9c265a2d12326
Cyrille Rossant <cyrille.rossant@github.com>
2013-02-05 17:13:16 +0000

    updated texture_reinit_test

--- galry/test/texture_reinit_test.py
+++ galry/test/texture_reinit_test.py
@@ -6,19 +6,23 @@ import numpy as np
 class PM(PaintManager):
     def initialize(self):
         self.add_visual(TextureVisual, texture=np.zeros((2, 2, 3)))
+        self.set_data(texture=np.zeros((2, 2, 3)))
         # initialize the visual again, but takes only the data defined in
         # visual.initialize()
+        self.update_test()
+
+    def update_test(self):
         tex = np.zeros((600, 600, 3))
         a = 150
         tex[a:-a,a:-a,:] = 1
         tex[a+1:-a-1,a+1:-a-1,:] = 0
         self.reinitialize_visual(texture=tex)
-
+        
 class PlotReinitTest(GalryTest):
     def test(self):
         self.show(paint_manager=PM)
 
 if __name__ == '__main__':
     unittest.main()
-    # show_basic_window(paint_manager=PM)
+    # w = show_basic_window(paint_manager=PM)
     
