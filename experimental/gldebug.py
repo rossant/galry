a16e283f5b5110a4ae79a7d55cb357a3bad0ba85
Cyrille Rossant <cyrille.rossant@github.com>
2013-03-22 15:43:00 +0000

    updated gldebug.py file

--- experimental/gldebug.py
+++ experimental/gldebug.py
@@ -11,17 +11,36 @@ Instructions:
 
 """
 import os
+import sys
 import pprint
+
 import numpy as np
+
 from galry import *
+
+# Capture stdout and stderr
+class writer(object):
+    log = []
+    def write(self, data):
+        self.log.append(data)
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            for line in self.log:
+                f.write(line)
+
+logger = writer()
+sys.stdout = logger
+sys.stderr = logger
 
 class DebugVisual(PlotVisual):
     def initialize(self):
         super(DebugVisual, self).initialize(np.zeros(2))
-        filename = os.path.realpath('./galry_gldebug.txt')
-        with open(filename, 'w') as f:
-            pprint.pprint(GLVersion.get_renderer_info(), f)
+        pprint.pprint(GLVersion.get_renderer_info())
         
 figure(autodestruct=1)
 visual(DebugVisual)
 show()
+
+filename = os.path.realpath('./galry_gldebug.txt')
+logger.save(filename)
+
