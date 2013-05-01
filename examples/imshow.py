Fixed normalization bug in TextureVisual.

examples/imshow.py
@@ -1,6 +1,7 @@
 from galry import *
 import numpy as np
 
-imshow(np.random.rand(100, 50))
+x = 10 * np.random.rand(100, 50)
+imshow(x)
 
 show()